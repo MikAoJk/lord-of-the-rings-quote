4999941c87b3e37d5a3cf5566d48b96e0a76837c
Joakim Taule Kartveit <joakimkartveit@gmail.com>
2025-10-19 12:38:27 +0200

    chore: clean up

--- src/pages/api/quotes.ts
+++ src/pages/api/quotes.ts
@@ -12,19 +12,10 @@ export default async function handler(
     req: NextApiRequest,
     res: NextApiResponse<Array<Quote>>
 ) {
-    if (process.env.NODE_ENV !== 'production') {
         const jsonDirectory = path.join(process.cwd(), 'src/json');
         const json = await fs.readFile(jsonDirectory + '/data.json', 'utf8');
 
         const quotes: Array<Quote> = JSON.parse(json);
 
         res.status(200).json(quotes);
-    } else {
-
-        const result = await fetch(`https://type.fit/api/quotes`, {
-            method: req.method
-        })
-
-        res.status(result.status).json(await result.json());
-    }
 }
